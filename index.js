b3094de66c38afe2c4e93671b0e2e378ac892224
Jakko Sikkar <jakko.sikkar@gmail.com>
2016-07-07 08:55:27 +0300

    add bootstrap and jquery

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// redirects
+app.use('/', express.static(__dirname + '/wwwroot'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
+// routing
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile('index.html');
 });
 
+// sockets.io
 io.on('connection', function(socket) {
     console.log('user connected...');
 
@@ -18,6 +27,7 @@ io.on('connection', function(socket) {
     });
 });
 
+// start server
 http.listen(3000, function() {
     console.log('Listening on *:3000...');
 });
