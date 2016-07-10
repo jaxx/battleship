e06b3f48dccebcb5a9f7019d46ef7c331c0279f9
Jakko Sikkar <jakko.sikkar@gmail.com>
2016-07-10 14:40:49 +0300

    same style to all javascript strings

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-var fs = require('fs');
+var fs = require("fs");
 
 function getVersion() {
-    var file = __dirname + '/VERSION';
-    var defaultVersion = '0.1.0-000001';
+    var file = __dirname + "/VERSION";
+    var defaultVersion = "0.1.0-000001";
 
     if (fs.existsSync(file)) {
-        return fs.readFileSync(file, 'utf8').trim();
+        return fs.readFileSync(file, "utf8").trim();
     }
 
     fs.writeFileSync(file, defaultVersion, 'utf8');
--- server.js
+++ server.js
@@ -1,48 +1,48 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var process = require('process');
-var config = require('./config');
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+var process = require("process");
+var config = require("./config");
 var moment = require("moment");
 
 // redirects
-app.use('/', express.static(__dirname + '/wwwroot'));
-app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
-app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
-app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
-app.use('/fonts', express.static(__dirname + '/node_modules/bootstrap/fonts'));
+app.use("/", express.static(__dirname + "/wwwroot"));
+app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js"));
+app.use("/js", express.static(__dirname + "/node_modules/jquery/dist"));
+app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
+app.use("/fonts", express.static(__dirname + "/node_modules/bootstrap/fonts"));
 
 // routing
-app.get('/', function(req, res) {
-    res.sendFile('index.html');
+app.get("/", function(req, res) {
+    res.sendFile("index.html");
 });
 
-console.log('Battleship server: v%s', config.version);
+console.log("Battleship server: v%s", config.version);
 
 function getts() {
     return moment().format("DD.MM.YYYY HH:mm:ss");
 }
 
 // sockets.io
-io.on('connection', function(socket) {
-    console.log('connection opened [%s] ...', socket.id);
-    socket.emit('server version', config.version);
+io.on("connection", function(socket) {
+    console.log("connection opened [%s] ...", socket.id);
+    socket.emit("server version", config.version);
 
-    socket.on('identify', function(username) {
-        socket.removeAllListeners('identify');
+    socket.on("identify", function(username) {
+        socket.removeAllListeners("identify");
 
         console.log("user identified: %s [%s]", username, socket.id);
-        io.emit('user connected', { username: username, time: getts() });
+        io.emit("user connected", { username: username, time: getts() });
 
-        socket.on('disconnect', function() {
-            console.log('user `%s` disconnected', username);
-            io.emit('user disconnected', { username: username, time: getts() });
+        socket.on("disconnect", function() {
+            console.log("user `%s` disconnected", username);
+            io.emit("user disconnected", { username: username, time: getts() });
         });
 
-        socket.on('chat message', function(text) {
+        socket.on("chat message", function(text) {
             if (text.trim()) {
-                io.emit('chat message', { username: username, text: text, time: getts() });
+                io.emit("chat message", { username: username, text: text, time: getts() });
             }
         });
     });
@@ -53,7 +53,7 @@ http.listen(3000, function() {
     console.log('Listening on *:3000...');
 });
 
-process.on('SIGINT', function() {
-    io.emit('server closed');
+process.on("SIGINT", function() {
+    io.emit("server closed");
     process.exit(0);
 });
